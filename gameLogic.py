1649195a1b6557b18a8b4b318e1c1877cb0e42de
孙翼 <38261135@qq.com>
2024-05-07 06:20:23 -0600

    update

--- gameLogic.py
+++ gameLogic.py
@@ -13,8 +13,15 @@ class GameLogic:
     def load_scenario(self, scenario):
         self.scenario = scenario
 
-    def create_entity(self, entity_id, entity_type, position):
-        self.entities[entity_id] = {"type": entity_type, "position": position}
+    def create_entity(self, entity_id, entity_type, position, speed, faction, hp, attack_power):
+        self.entities[entity_id] = {
+            "type": entity_type,
+            "position": position,
+            "speed": speed,
+            "faction": faction,
+            "hp": hp,
+            "attack_power": attack_power
+        }
 
     def delete_entity(self, entity_id):
         if entity_id in self.entities:
@@ -54,9 +61,39 @@ class GameLogic:
         self.entities[entity_id]['position'] = new_position
         print(f"Entity {entity_id} moved to {new_position}")
 
-    def detect_entities(self):
-        # 简化的探测逻辑
-        return {eid: ent['position'] for eid, ent in self.entities.items()}
+    def detect_entities(self, entity_id, detection_range):
+        if entity_id not in self.entities:
+            return {}
+
+        current_position = np.array(self.entities[entity_id]['position'])
+        visible_entities = {}
+        for other_id, data in self.entities.items():
+            if other_id != entity_id:
+                other_position = np.array(data['position'])
+                if np.linalg.norm(current_position - other_position) <= detection_range:
+                    visible_entities[other_id] = data
+        return visible_entities
+
+    def attack(self, attacker_id, target_id, attack_range):
+        if attacker_id not in self.entities or target_id not in self.entities:
+            return "Invalid entity"
+
+        attacker = self.entities[attacker_id]
+        target = self.entities[target_id]
+
+        # 检查目标是否在攻击范围内
+        attacker_pos = np.array(attacker['position'])
+        target_pos = np.array(target['position'])
+        if np.linalg.norm(attacker_pos - target_pos) > attack_range:
+            return "Target out of range"
+
+        # 执行攻击
+        damage = attacker['attack_power']
+        target['hp'] -= damage
+        if target['hp'] <= 0:
+            self.delete_entity(target_id)
+            return f"Target {target_id} destroyed"
+        return f"Attacked {target_id}, {damage} damage dealt"
 
     def step(self, actions):
         # 处理动作，更新状态
